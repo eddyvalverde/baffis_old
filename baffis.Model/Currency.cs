228d95f32dd04e94da2a88c2266c19f81a26d7bf
eddygarros <eddygarros@hotmail.com>
2022-03-02 18:00:22 -0600

    fix: add country

--- baffis.Model/Currency.cs
+++ baffis.Model/Currency.cs
@@ -11,5 +11,6 @@ namespace baffis.Model
         public int IdCurrency { get; set; }
         public string Name { get; set; }
         public string SYMBOL { get; set; }
+        public string Country { get; set; }
     }
 }
