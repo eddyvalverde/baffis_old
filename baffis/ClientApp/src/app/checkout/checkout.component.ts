6631c272e2e5a1d38b30de576d6dc1f5892197a8
eddygarros <eddygarros@hotmail.com>
2022-03-11 11:35:42 -0600

    feature: add conection with checout api

--- baffis/ClientApp/src/app/checkout/checkout.component.ts
+++ baffis/ClientApp/src/app/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { StripeService } from 'ngx-stripe';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private stripeService: StripeService  
+  ) { }
 
   ngOnInit() {
+  }
+
+  checkout() {
+    // Check the server.js tab to see an example implementation
+    this.http.post<any>('https://localhost:44387/create-checkout-session', {})
+      .pipe(
+        switchMap(session => {
+          return this.stripeService.redirectToCheckout({ sessionId: session.id })
+        })
+      )
+      .subscribe(result => {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, you should display the localized error message to your
+        // customer using `error.message`.
+        if (result.error) {
+          alert(result.error.message);
+        }
+      });
   }
 
 }
