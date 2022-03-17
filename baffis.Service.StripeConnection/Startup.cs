7b963dd21b43c0965efc542a5fe1681345d1edbe
Eddy Garro S <eddygarros@hotmail.com>
2022-03-17 14:25:31 -0600

    fix: fix

--- baffis.Service.StripeConnection/Startup.cs
+++ baffis.Service.StripeConnection/Startup.cs
@@ -47,7 +47,7 @@ namespace baffis.Service.StripeConnection
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline,
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // This is your test secret API key.
